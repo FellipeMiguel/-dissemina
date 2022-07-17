08837e80417afca844956e22754919ae51db20cb
RayaniGomes <68917577+RayaniGomes@users.noreply.github.com>
2022-07-16 22:40:45 -0300

    Delete main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const menumobile = document.getElementById('menumobile');
-
-function toglleMenu(){
-    const nav = document.getElementById('navbar');
-    navbar.classList.toggle('active');
-}
-menumobile.addEventListener('click', toglleMenu);
